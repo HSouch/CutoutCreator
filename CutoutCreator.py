9cc69e054f4cc3ae88a87db4bbf545e68d2fba17
Harrison Souchereau <hsouchereau@outlook.com>
2020-04-22 20:52:04 -0300

    Updated file getter and added multiprocessing

--- CutoutCreator.py
+++ CutoutCreator.py
@@ -10,6 +10,9 @@ try:
     import warnings
     import math
     import logging
+    from pathlib import Path
+
+    import multiprocessing as mp
 
     from astropy.io import fits
     from astropy.table import Table
@@ -98,63 +101,6 @@ def get_arc_conv(w: wcs.WCS):
     return (diff_1 + diff_2) / 2
 
 
-def get_angular_size_dist(z, h0=71, wm=0.27, wv=None):
-    """
-    Return the angular size distance in Megaparsecs.
-    (Stripped down version of Cosmocalc by Ned Wright and Tom Aldcroft (aldcroft@head.cfa.harvard.edu))
-    """
-    try:
-        c = 299792.458  # velocity of light in km/sec
-
-        if z > 100:
-            z /= 299792.458  # Values over 100 are in km/s
-
-        if wv is None:
-            wv = 1.0 - wm - 0.4165 / (h0 * h0)  # Omega(vacuum) or lambda
-        age = 0.0  # age of Universe in units of 1/H0
-
-        h = h0 / 100.
-        wr = 4.165E-5 / (h * h)  # includes 3 massless neutrino species, T0 = 2.72528
-        wk = 1 - wm - wr - wv
-        az = 1.0 / (1 + 1.0 * z)
-        n = 1000  # number of points in integrals
-        for i in range(n):
-            a = az * (i + 0.5) / n
-            adot = math.sqrt(wk + (wm / a) + (wr / (a * a)) + (wv * a * a))
-            age += 1. / adot
-
-        dcmr = 0.0
-
-        # do integral over a=1/(1+z) from az to 1 in n steps, midpoint rule
-        for i in range(n):
-            a = az + (1 - az) * (i + 0.5) / n
-            adot = math.sqrt(wk + (wm / a) + (wr / (a * a)) + (wv * a * a))
-            dcmr = dcmr + 1. / (a * adot)
-
-        dcmr = (1. - az) * dcmr / n
-
-        # tangential comoving distance
-        ratio = 1.0
-        x = math.sqrt(abs(wk)) * dcmr
-        if x > 0.1:
-            if wk > 0:
-                ratio = 0.5 * (math.exp(x) - math.exp(-x)) / x
-            else:
-                ratio = math.math.sin(x) / x
-        else:
-            y = x * x
-            if wk < 0:
-                y = -y
-            ratio = 1. + y / 6. + y * y / 120.
-        dcmt = ratio * dcmr
-        da = az * dcmt
-        mpc = lambda x: c / h0 * x
-        da_mpc = mpc(da)
-
-        return da_mpc
-    except:
-        raise ValueError
-
 
 def generate_cutout(image, position, img_wcs=None, size=91, world_coords=True):
     """
@@ -167,11 +113,6 @@ def generate_cutout(image, position, img_wcs=None, size=91, world_coords=True):
     :param world_coords:
     :return:
     """
-
-    if size < 71:
-        size = 71
-    elif size % 2 == 0:
-        size += 1
 
     if world_coords:
         coord = img_wcs.wcs_world2pix(position[0], position[1], 0, ra_dec_order=True)
@@ -190,41 +131,55 @@ def generate_cutout(image, position, img_wcs=None, size=91, world_coords=True):
         raise IndexError
 
 
-def mask_cutout(cutout):
+def mask_cutout(cutout, nsigma=1., gauss_width=2.0, npixels=5):
     """ Masks a cutout using segmentation and deblending using watershed"""
-
-    # General container for mask information
     mask_data = {}
 
+    # Generate a copy of the cutout just to prevent any weirdness with numpy pointers
     cutout_copy = copy(cutout)
 
-    extraction_sigma = 2.0 * gaussian_fwhm_to_sigma
-    extraction_kernel = Gaussian2DKernel(extraction_sigma)
-    extraction_kernel.normalize()
+    sigma = gauss_width * gaussian_fwhm_to_sigma
+    kernel = Gaussian2DKernel(sigma)
+    kernel.normalize()
 
-    # Generate Thresholds for profile extraction and background estimation
-    extraction_threshold = detect_threshold(cutout, snr=1.)
+    # Find threshold for cutout, and make segmentation map
+    threshold = detect_threshold(cutout, snr=nsigma)
+    segments = detect_sources(cutout, threshold, npixels=npixels,
+                              filter_kernel=kernel)
 
-    segments = detect_sources(cutout, extraction_threshold, npixels=5, filter_kernel=extraction_kernel)
-
-    # Attempt to deblend. Don't do anything and continue with original segmentation map upon failure.
+    # Attempt to deblend. Return original segments upon failure.
     try:
-        deb_segments = deblend_sources(cutout, segments, npixels=5, filter_kernel=extraction_kernel)
+        deb_segments = deblend_sources(cutout, segments, npixels=5,
+                                       filter_kernel=kernel)
+    except ImportError:
+        print("Skimage not working!")
+        deb_segments = segments
     except:
+        # Don't do anything if it doesn't work
         deb_segments = segments
 
-    # Prepare segment array (pull out the raw data from the deblended segmentation object)
     segment_array = deb_segments.data
 
-    # Center pixel values
+    # Center pixel values. (Assume that the central segment is the image, which is should be)
     c_x, c_y = floor(segment_array.shape[0] / 2), floor(segment_array.shape[1] / 2)
     central = segment_array[int(c_x)][int(c_y)]
 
-    bg_estimate, bg_rms = estimate_background(cutout)
+    # Estimate Background, and min/max values
+    bg_total, bg_pixels, bg_pixel_array = 0, 0, []
+    min_val, max_val, = cutout_copy[0][0], cutout_copy[0][0]
+    for x in range(0, segment_array.shape[0]):
+        for y in range(0, segment_array.shape[1]):
+            if segment_array[x][y] == 0:
+                bg_total += cutout_copy[x][y]
+                bg_pixels += 1
+                bg_pixel_array.append(cutout_copy[x][y])
 
+    bg_estimate = bg_total / bg_pixels
     mask_data["BG_EST"] = bg_estimate
-    mask_data["BG_RMS"] = bg_rms
+    mask_data["BG_MED"] = median(bg_pixel_array)
     mask_data["N_OBJS"] = segments.nlabels
+    mask_data["MIN_VAL"] = min_val
+    mask_data["MAX_VAL"] = max_val
 
     # Return input image if no need to mask
     if segments.nlabels == 1:
@@ -241,6 +196,7 @@ def mask_cutout(cutout):
     mask_data["N_MASKED"] = num_masked
 
     return cutout_copy, mask_data
+
 
 
 def estimate_background(cutout: ndarray):
@@ -305,6 +261,7 @@ def estimate_background(cutout: ndarray):
 
 global_verbose = False
 global_batch = False
+global_multithread = False
 has_tract_info = False
 global_mask = False
 cutout_size = 41
@@ -320,6 +277,7 @@ def process_arguments():
     """ Gather all user arguments, and set global variables if necessary. """
     global global_verbose
     global global_batch
+    global global_multithread
     global global_mask
     global has_tract_info
     global test_mode
@@ -345,8 +303,12 @@ def process_arguments():
                         action="store_true")
 
     parser.add_argument("-b", "--batch",
-                        help="Run Koe in batch mode. Will assume image_filename is a path containing many " +
+                        help="Run in batch mode. Will assume image_filename is a path containing many " +
                         "images.",
+                        action="store_true")
+
+    parser.add_argument("--multi",
+                        help="Run in multithread mode.",
                         action="store_true")
 
     parser.add_argument("-m", "--mask",
@@ -369,7 +331,7 @@ def process_arguments():
     args = parser.parse_args()
 
     # Main Introduction Message
-    if args.verbose or args.extra_verbose:
+    if args.verbose:
         print("\n\tCutout Creator -- Written by Harrison Souchereau -- hsouchereau@outlook.com \n")
         global_verbose = True
 
@@ -378,10 +340,14 @@ def process_arguments():
         has_tract_info = True
     if args.batch:
         global_batch = True
+    if args.multi:
+        global_multithread = True
     if args.output_subdir is not None:
         # Add "/" to output subdirectory if it doesn't exist.
         output_subdirectory = args.output_subdir
         output_subdirectory += "/" if output_subdirectory[len(output_subdirectory) - 1] is not "/" else ""
+    if args.mask is not None:
+        global_mask = args.mask
     if args.ra is not None:
         ra_key = args.ra
     if args.dec is not None:
@@ -412,24 +378,31 @@ def process(img_filename, cat_filename):
 
     # Gather all images to process (make an array of 1 object if not in batch mode)
     if global_batch:
-        images = os.listdir(img_filename)
-        for n in range(0, len(images)):
-            images[n] = img_filename + images[n]
+        images = []
+        images_raw = Path(img_filename).rglob('*.fits')
+        for image in images_raw:
+            images.append(str(image))
     else:
         images = [img_filename]
 
-    for image in images:
-        process_image(cat, image)
+    if global_multithread:
+        print("Multiprocessing Selected:", mp.cpu_count(), "threads available.")
+        pool = mp.Pool(processes=len(images))
+        results = [pool.apply_async(process_image, (cat, image)) for image in images]
+        [res.get() for res in results]
+    else:
+        for image in images:
+            process_image(cat, image)
 
     return None
 
 
-def process_image(catalogue, img_filename):
+def process_image(catalogue, img_filename, cutout_size=cutout_size):
     """ Run fitting routine on an image. """
 
     # Get the filename of the image without any path information
     img_filename_no_path = img_filename.split("/")[len(img_filename.split("/")) - 1]
-
+    print("Processing:\t", img_filename_no_path)
     # Check for tract info, and gather objects that will be considered for fitting ###################################
     valid_indices = range(0, len(catalogue))
     if has_tract_info:
@@ -467,7 +440,6 @@ def process_image(catalogue, img_filename):
 
     # Process Objects ################################################################################################
     for index in valid_indices:
-        print(index)
         extra_params = {}   # Container dict for all local paramters (to add to output header)
         row = catalogue[index]
 
@@ -476,21 +448,27 @@ def process_image(catalogue, img_filename):
 
         # Make cutout
         try:
-            cutout_size = 31
             cutout = generate_cutout(img, (row[ra_key], row[dec_key]),
                                      size=cutout_size, img_wcs=w)
+            if cutout.shape[0] != cutout.shape[1]:
+                print(cutout.shape)
+                continue
         except (IndexError, ValueError):
             # Move on to the next objcet if outside of the image boundaries
             continue
 
         # Mask cutout
-        try:
-            masked_cutout, mask_data = mask_cutout(cutout)
-            extra_params.update(mask_data)
-        except ValueError:
-            if global_verbose:
-                print("Error fitting galaxy", index, "due to masking failure.")
-            continue
+        if global_mask:
+            print("yes")
+            try:
+                masked_cutout, mask_data = mask_cutout(cutout)
+
+                cutout = masked_cutout
+                extra_params.update(mask_data)
+            except ValueError:
+                if global_verbose:
+                    print("Error processing galaxy", index, "due to masking failure.")
+                continue
 
         hdr = fits.Header()
         for column in row.colnames:
